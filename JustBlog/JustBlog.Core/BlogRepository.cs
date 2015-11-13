b2a3d24c69bdfa092c360f7f11598b9967150120
Ted Klikas <ted.klikas@reedonline.co.uk>
2015-11-13 16:03:59 +0000

    added data-access classes

---
+++ JustBlog/JustBlog.Core/BlogRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JustBlog.Core.Objects;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace JustBlog.Core
+{
+    class BlogRepository : IBlogRepository
+    {
+        //NHibernate object
+        private readonly ISession _session ;
+
+        public BlogRepository(ISession session)
+        {
+            _session = session;
+        }
+
+        /*
+         * All the calls to the database has to be made through the NHibernate's ISession object. 
+         * When we read the collection of posts using ISession, the dependencies Category and Tags are not populated by default. 
+         * Fetch and FetchMany methods are used to tell NHibernate to populate them eagerly.
+         */
+        public IList<Post> Posts(int pageNo, int pageSize)
+        {
+            var posts = _session.Query<Post>()
+                .Where(p => p.Published)
+                .OrderByDescending(p => p.PostedOn)
+                .Skip(pageNo*pageSize)
+                .Fetch(p => p.Category)
+                .ToList();
+
+            var postIds = posts.Select(p => p.Id).ToList();
+
+            return _session.Query<Post>()
+                .Where(p => postIds.Contains(p.Id))
+                .OrderByDescending(p => p.PostedOn)
+                .FetchMany(p => p.Tags)
+                .ToList();
+        }
+
+        public int TotalPosts()
+        {
+            return _session.Query<Post>().Count(p => p.Published);
+        }
+    }
+}
---
+++ JustBlog/JustBlog.Core/Mappings/PostMap.cs
@@ -0,0 +1,69 @@
+﻿using FluentNHibernate.Mapping;
+using JustBlog.Core.Objects;
+
+namespace JustBlog.Core.Mappings
+{
+    /* 
+    * To create a mapping class we should inherit it from the Fluent NHibernate's generic class ClassMap. 
+    * All the mappings has to be done in the constructor.
+    * As default, Fluent NHibernate assumes the table name as same as the class name and column name as same as the property name. 
+    * If the table name is different then we should map the table with the class using the Table extension method.
+    * [e.g Table("tbl_posts");]
+    */
+    class PostMap: ClassMap<Post>
+    {
+        public PostMap()
+        {
+            /* The Id extension method is used to represent the property name that has to be set as the primary key column of the table. */
+            Id(x => x.Id);
+            /* 
+             * The Map extension method is used to map a property with a table column. 
+             * While mapping a property we can specify the size of the column, whether it's nullable or not and other details. 
+             * If the generated column name has to be different from the property name then we should pass the column name using the Column extension method.
+             * 
+             * [e.g Map(x => x.Title).Column("post_title")]
+             * 
+             */
+            Map(x => x.Title)
+                .Length(500)
+                .Not.Nullable();
+
+            Map(x => x.ShortDescription)
+                .Length(5000)
+                .Not.Nullable();
+
+            Map(x => x.Description)
+                .Length(5000)
+                .Not.Nullable();
+
+            Map(x => x.Meta)
+                .Length(1000)
+                .Not.Nullable();
+
+            Map(x => x.UrlSlug)
+                .Length(200)
+                .Not.Nullable();
+
+            Map(x => x.Published)
+                .Not.Nullable();
+
+            Map(x => x.PostedOn)
+               .Not.Nullable();
+
+            Map(x => x.Modified);
+
+            /*
+             * The References method is used to represent the many-to-one relationship between Post and Category 
+             * through a foreign key column Category in the Post table. 
+             * HasManyToMany method is used to represent many-to-many relationship between Post and Tag 
+             * and this is achieved through an intermediate table called PostTagMap.
+             */
+            References(x => x.Category)
+               .Column("Category")
+               .Not.Nullable();
+
+            HasManyToMany(x => x.Tags)
+              .Table("PostTagMap");
+        }
+    }
+}
